579355c9d611ca5b75c9ef93a45ccb328a80b1af
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-11 02:16:09 +0530

    Added:Function to return Custom Exception If Wrong File Name Given.

--- src/main/java/com/bridgelabz/StateCodeAnalyser.java
+++ src/main/java/com/bridgelabz/StateCodeAnalyser.java
@@ -4,6 +4,7 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import static java.nio.file.Files.newBufferedReader;
@@ -13,14 +14,12 @@ public class StateCodeAnalyser {
     private static String CSV_STATE_CODE_FILE_PATH = "./src/test/resources/StateCode.csv";
     int totalNumberOfRecords = 0;
 
-    public StateCodeAnalyser(String path)
-    {
+    public StateCodeAnalyser(String path) {
         CSV_STATE_CODE_FILE_PATH = path;
     }
 
-    public int loadStateCodeData() throws IOException {
-        try (Reader reader = newBufferedReader(Paths.get(CSV_STATE_CODE_FILE_PATH)))
-        {
+    public int loadStateCodeData() throws StateCensusException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(CSV_STATE_CODE_FILE_PATH));) {
             CsvToBean<CSVStateCode> csvStateCodeBean = new CsvToBeanBuilder(reader)
                     .withType(CSVStateCode.class)
                     .withIgnoreLeadingWhiteSpace(true)
@@ -28,14 +27,14 @@ public class StateCodeAnalyser {
             Iterator<CSVStateCode> csvStateCodeIterator = csvStateCodeBean.iterator();
             while (csvStateCodeIterator.hasNext()) {
                 CSVStateCode csvStateCode = csvStateCodeIterator.next();
-                System.out.println("SrNo :" +csvStateCode.getSrNo());
-                System.out.println("StateName :" +csvStateCode.getStateName());
-                System.out.println("TIN :" +csvStateCode.getTIN());
-                System.out.println("StateCode :" +csvStateCode.getStateCode());
+                System.out.println("SrNo :" + csvStateCode.getSrNo());
+                System.out.println("StateName :" + csvStateCode.getStateName());
+                System.out.println("TIN :" + csvStateCode.getTIN());
+                System.out.println("StateCode :" + csvStateCode.getStateCode());
                 totalNumberOfRecords++;
             }
-        }catch (IOException e) {
-            System.out.println("Error");
+        } catch (IOException e) {
+            throw new StateCensusException(StateCensusException.TypeOfException.NO_FILE_FOUND, "No Such File Present");
         }
         return totalNumberOfRecords;
     }
--- src/test/java/com/bridgelabz/TestStateCensus.java
+++ src/test/java/com/bridgelabz/TestStateCensus.java
@@ -10,6 +10,7 @@ public class TestStateCensus {
     public static String CSV_FILE_PATH_FOR_WRONG_FILE = "./src/test/resources/StateCensus.csv";
     public static String CSV_FILE_PATH_FOR_WRONG_FILE_EXTENSION = "./src/test/resources/StateCensus.jpg";
     public static String CSV_STATES_CODE_FILE_PATH = "./src/test/resources/StateCode.csv";
+    public static String CSV_WRONG_STATES_CODE_FILE_PATH = "./src/test/resources/WrongStateCode.csv";
 
     @Test
     public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws StateCensusException {
@@ -67,9 +68,24 @@ public class TestStateCensus {
     }
 
     @Test
-    public void givenStateCode_WhenTrue_ReturnNumberOfRecordMatch() throws IOException {
+    public void givenStateCode_WhenTrue_ReturnNumberOfRecordMatch() throws StateCensusException {
         StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser(CSV_STATES_CODE_FILE_PATH);
         int noOfRecords=stateCodeAnalyser.loadStateCodeData();
         Assert.assertEquals(37, noOfRecords);
     }
+
+    @Test
+    public void givenStateCodeWhenFalse_ReturnExceptionFileNotFound() throws StateCensusException
+    {
+        StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser(CSV_WRONG_STATES_CODE_FILE_PATH);
+        try
+        {
+            stateCodeAnalyser.loadStateCodeData();
+        }
+        catch (StateCensusException e)
+        {
+            Assert.assertEquals(StateCensusException.TypeOfException.NO_FILE_FOUND,e.type);
+        }
+    }
+
 }
