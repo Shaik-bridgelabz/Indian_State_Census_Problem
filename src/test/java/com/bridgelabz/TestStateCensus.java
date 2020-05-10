4e7c992ac175b0c6762b0c30d2257ef4183fc7ab
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-11 00:39:36 +0530

    Added: TestCase to Check Improper File Extension.

--- src/test/java/com/bridgelabz/TestStateCensus.java
+++ src/test/java/com/bridgelabz/TestStateCensus.java
@@ -8,6 +8,7 @@ public class TestStateCensus {
 
     public static String CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
     public static String CSV_FILE_PATH_FOR_WRONG_FILE = "./src/test/resources/StateCensus.csv";
+    public static String CSV_FILE_PATH_FOR_WRONG_FILE_EXTENSION = "./src/test/resources/StateCensus.jpg";
 
     @Test
     public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws StateCensusException {
@@ -17,14 +18,23 @@ public class TestStateCensus {
     }
 
     @Test
-    public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() throws StateCensusException {
-        try
-        {
+    public void givenStateCensusDataFile_WhenImproperFileName_ReturnsException() throws StateCensusException {
+        try {
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(CSV_FILE_PATH_FOR_WRONG_FILE);
             stateCensusAnalyser.loadData();
         }
-        catch (StateCensusException e)
-        {
+        catch (StateCensusException e) {
+            Assert.assertEquals(StateCensusException.TypeOfException.NO_FILE_FOUND,e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCensusDataFile_WhenImproperFileExtension_ReturnsException() throws StateCensusException {
+        try {
+            StateCensusAnalyser censusAnalyserObject = new StateCensusAnalyser(CSV_FILE_PATH_FOR_WRONG_FILE_EXTENSION);
+            censusAnalyserObject.loadData();
+        }
+        catch (StateCensusException e) {
             Assert.assertEquals(StateCensusException.TypeOfException.NO_FILE_FOUND,e.type);
         }
     }
